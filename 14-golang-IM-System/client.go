28e59274b7ca2fea23471d37173288408ef3a936
hardenzheng <hardenzheng@tencent.com>
2021-11-23 21:21:12 +0800

    feat: 客户端可输入自定义ip 和端口号

--- 14-golang-IM-System/client.go
+++ 14-golang-IM-System/client.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"net"
 )
@@ -29,8 +30,20 @@ func NewClient(serverIp string, serverPort int) *Client{
 	return client
 }
 
+var serverIp string
+var serverPort int
+
+//./client -ip 127.0.0.1
+func init(){
+	flag.StringVar(&serverIp, "ip","127.0.0.1", "设置服务器ip地址")
+	flag.IntVar(&serverPort, "port",8888, "设置服务器端口")
+}
+
 func main() {
-	client := NewClient("127.0.0.1", 8888)
+	//命令行解析
+	flag.Parse()
+
+	client := NewClient(serverIp, serverPort)
 	if client == nil{
 		fmt.Println("连接服务器失败...")
 		return
