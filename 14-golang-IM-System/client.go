721e0b6c7cf26a1b797b5960ca2393b74a157c04
hardenzheng <hardenzheng@tencent.com>
2021-11-24 13:14:48 +0800

    feat：添加菜单

--- 14-golang-IM-System/client.go
+++ 14-golang-IM-System/client.go
@@ -11,6 +11,7 @@ type Client struct {
 	ServerPort int
 	Name string
 	conn net.Conn
+	flag int // 当前client的模式
 }
 
 func NewClient(serverIp string, serverPort int) *Client{
@@ -18,6 +19,7 @@ func NewClient(serverIp string, serverPort int) *Client{
 	client := &Client{
 		ServerIp: serverIp,
 		ServerPort: serverPort,
+		flag: -1,
 	}
 //	连接server
 	 conn, err := net.Dial("tcp", fmt.Sprintf("%s:%d", serverIp, serverPort))
@@ -29,6 +31,46 @@ func NewClient(serverIp string, serverPort int) *Client{
 //	返回对象
 	return client
 }
+
+func (client *Client) menu() bool{
+	var flag int
+
+	fmt.Println("1.公聊模式")
+	fmt.Println("2.私聊模式")
+	fmt.Println("3.更新用户名")
+	fmt.Println("0.退出")
+
+	fmt.Scanln(&flag)
+
+	if flag >= 0 && flag <= 3{
+		client.flag = flag
+		return true
+	} else {
+		fmt.Println(">>>请输入合法范围内的数字<<<")
+		return false
+	}
+}
+
+func (client *Client) Run(){
+	for client.flag != 0{
+		for client.menu() != true {
+		}
+	//	根据不同的模式处理不同的业务
+		switch client.flag {
+		case 1:
+			fmt.Println("choose 公聊")
+			break
+		case 2:
+			fmt.Println("choose 私聊")
+			break
+		case 3:
+			fmt.Println("choose 更新用户名")
+			break
+		}
+	}
+}
+
+
 
 var serverIp string
 var serverPort int
@@ -52,6 +94,5 @@ func main() {
 	fmt.Println("连接服务器成功...")
 
 //	启动客户端的业务
-	select {
-	}
+	client.Run()
 }
